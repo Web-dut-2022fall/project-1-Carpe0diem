cd2f565bf0c6d7cd6e87a62715d8d34b8aca45d7
Carpe0diem <98384063+Carpe0diem@users.noreply.github.com>
2023-01-05 00:40:58 +0800

    1

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("search",views.search,name="search"),
+    path("new",views.new_page,name="new_page"),
+    path("random",views.random,name="random"),
+    path("wiki/<str:name>",views.article, name="article"),
+    path("wiki/<str:name>/edit", views.edit, name="edit")
+
 ]
